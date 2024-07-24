e74120149adca08874b9124e34bddf6972d2ef86
Axuata <axuata@gmail.com>
2024-07-24 11:32:25 +0900

    fix: Changed URLs

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,7 @@
 <template>
   <PagesHomeWelcome />
   <GlobalSection title="Projects">
-    <GlobalCard card-title="Lunorix Materials" card-description="Lunorix Materials (Images and so on)" card-url="https://kamigame.jp/bluearchive/page/142932339821927927.html" card-image="/assets/images/lunorix/banner/banner.png" cardImageAlt="Lunorix Materials Image" />
-    <GlobalCard card-title="Lunorix" card-description="Lunorix Official Website" card-url="https://kamigame.jp/bluearchive/page/142932339821927927.html" card-image="/assets/website/images/projects/lunorix.png" cardImageAlt="Lunorix Materials Image" />
+    <GlobalCard card-title="Lunorix Materials" card-description="Lunorix Materials (Images and so on)" card-url="https://github.com/lunorix/.lunorix-materials" card-image="/assets/images/lunorix/banner/banner.png" cardImageAlt="Lunorix Materials Image" />
+    <GlobalCard card-title="Lunorix" card-description="Lunorix Official Website" card-url="https://github.com/lunorix/lunorix" card-image="/assets/website/images/projects/lunorix.png" cardImageAlt="Lunorix Materials Image" />
   </GlobalSection>
 </template>
